0ee11eb89d27fc77f230a71537f0cfda185fdd00
percenterror <0_david11_11@proton.me>
2025-02-15 07:48:14 +0000

    Add files via upload

---
+++ commands/random.js
@@ -0,0 +1,43 @@
+//const lib = require('fs');
+
+//readFile( Path, Options, Callback);
+
+//fs.readFile('links.txt', (err, data) => {
+//    if (err) throw err;
+//  
+//    console.log(data.toString());
+//});
+
+let list = ["link1", "link2", "link3"]
+
+console.log(list[Math.floor(Math.random() * (list.length - 1))])
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('image')
+        .setDescription('Sends a random image from a list of images.'),
+    async execute(interaction) {
+        try {
+            // Randomly select an image
+            const randomImage = list[Math.floor(Math.random() * (list.length - 1))]
+
+            // Create embed with the image
+            const embed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle('Hello!')
+                .setDescription('Here\'s an image:\n' + randomImage)
+                .setTimestamp();
+
+            // Send the response with the attached image
+            await interaction.reply({
+                embeds: [embed],
+            });
+
+            console.log('Successfully sent image:', randomImage); // Add logging
+        }
+        catch (error) {
+            console.error('Error executing the command:', error);
+            await interaction.reply({ content: 'There was an error executing this command!', ephemeral: true });
+        }
+    },
+};
